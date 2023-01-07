910c9e4a2894bade39980f7730883d6d94e13f05
NTiger07 <falaleru@gmail.com>
2023-01-07 23:08:55 +0100

    start

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,11 @@
 import React from "react";
-
+import Hero from "./Hero";
 import "../App.css";
 
 export default function App() {
-  return <div className="main_container"></div>;
+  return (
+    <div className="main_container">
+      <Hero />
+    </div>
+  );
 }
