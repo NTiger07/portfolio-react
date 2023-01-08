bc078b4ed625859736c0eedffef6d6a9e6e7e99e
NTiger07 <falaleru@gmail.com>
2023-01-08 20:54:01 +0100

    Projects Section

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import Hero from "./Hero";
+import About from "./About";
+import Projects from "./Projects";
 import "../App.css";
 
 export default function App() {
   return (
     <div className="main_container">
       <Hero />
+      <About />
+      <Projects />
     </div>
   );
 }
