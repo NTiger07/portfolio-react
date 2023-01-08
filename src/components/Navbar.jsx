4bdf26cc207c86d128af04c101004b1fec019816
NTiger07 <falaleru@gmail.com>
2023-01-08 20:49:00 +0100

    navbar start

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,6 +2,13 @@ import React, { Component } from "react";
 
 export default class Navbar extends Component {
   render() {
-    return <div>Navbar</div>;
+    return (
+      <div className="navbar_container">
+        <div className="logo_container">
+          <img src="" alt="logo" />
+        </div>
+        <div className="links_container"></div>
+      </div>
+    );
   }
 }
