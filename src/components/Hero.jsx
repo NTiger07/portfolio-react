96d5402917760271011658d58539e327339e6ec4
NTiger07 <falaleru@gmail.com>
2023-01-08 20:44:22 +0100

    hero gspeed

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,22 @@
 import React, { Component } from "react";
+import Navbar from "./Navbar";
 
 export default class Hero extends Component {
   render() {
-    return <div>I'm Favour Olaleru</div>;
+    return (
+      <div>
+        <Navbar />
+        <span>HI, MY NAME IS</span>
+        <h1>Favour Olaleru.</h1>
+        <span>I am a Front-End Developer.</span>
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iusto
+          pariatur repudiandae molestiae excepturi animi voluptatibus et
+          laudantium deleniti, ut rem totam facilis recusandae amet quis
+          incidunt voluptas. Ratione laborum magnam officiis quibusdam cum quo,
+          beatae modi quis nisi, aliquid atque eveniet animi at quod placeat!
+        </p>
+      </div>
+    );
   }
 }
