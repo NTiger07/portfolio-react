3ba5acfc0e4847e0a71758584e20f97f6267bdb1
NTiger07 <falaleru@gmail.com>
2023-01-08 21:21:15 +0100

    About Section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,7 +2,29 @@ import React, { Component } from "react";
 
 class About extends Component {
   render() {
-    return <div className="about_section"></div>;
+    return (
+      <div className="about_section">
+        <div className="about_section-image">
+          <img src="" alt="svgillustration" />
+        </div>
+        <div className="about_section-content">
+          <h2>ABOUT ME</h2>
+          <span>I'm Favour Olaleru</span>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam eum in
+            commodi obcaecati aspernatur reiciendis omnis at nisi saepe
+            asperiores, vero tempora suscipit voluptate vel officia facilis
+            delectus! Deserunt odit perferendis enim voluptatibus quisquam nemo.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam eum in
+            commodi obcaecati aspernatur reiciendis omnis at nisi saepe
+            asperiores, vero tempora suscipit voluptate vel officia facilis
+            delectus! Deserunt odit perferendis enim voluptatibus quisquam nemo.
+          </p>
+        </div>
+      </div>
+    );
   }
 }
 
