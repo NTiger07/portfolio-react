ed2d3acc1dec02ca215a08d8445b9a1ae8929e54
NTiger07 <falaleru@gmail.com>
2023-01-12 23:05:12 +0100

    About styling done

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,23 +6,27 @@ class About extends Component {
     return (
       <div className="about_section">
         <h1>ABOUT ME</h1>
-        <div className="about_section-image">
-          <img src="" alt="svgillustration" />
-        </div>
-        <div className="about_section-content">
-          <span>I'm Favour Olaleru</span>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam eum in
-            commodi obcaecati aspernatur reiciendis omnis at nisi saepe
-            asperiores, vero tempora suscipit voluptate vel officia facilis
-            delectus! Deserunt odit perferendis enim voluptatibus quisquam nemo.
-          </p>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam eum in
-            commodi obcaecati aspernatur reiciendis omnis at nisi saepe
-            asperiores, vero tempora suscipit voluptate vel officia facilis
-            delectus! Deserunt odit perferendis enim voluptatibus quisquam nemo.
-          </p>
+        <div className="row">
+          <div className="about_section-image">
+            <img src="" alt="svgillustration" />
+          </div>
+          <div className="about_section-content">
+            <span>I'm Favour Olaleru</span>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam eum
+              in commodi obcaecati aspernatur reiciendis omnis at nisi saepe
+              asperiores, vero tempora suscipit voluptate vel officia facilis
+              delectus! Deserunt odit perferendis enim voluptatibus quisquam
+              nemo.
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam eum
+              in commodi obcaecati aspernatur reiciendis omnis at nisi saepe
+              asperiores, vero tempora suscipit voluptate vel officia facilis
+              delectus! Deserunt odit perferendis enim voluptatibus quisquam
+              nemo.
+            </p>
+          </div>
         </div>
       </div>
     );
