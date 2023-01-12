9fc76ae9219a5f14cb64e0c9d46ea37fee319f8a
NTiger07 <falaleru@gmail.com>
2023-01-12 23:18:01 +0100

    resolved imports

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,4 +1,13 @@
 import React, { Component } from "react";
+import html from "../assets/HTML.svg";
+import css from "../assets/CSS.svg";
+import js from "../assets/JavaScript.svg";
+import ts from "../assets/TypeScript.svg";
+import react from "../assets/React-Dark.svg";
+import node from "../assets/NodeJS-Dark.svg";
+import git from "../assets/Git.svg";
+import github from "../assets/Github-Dark.svg";
+import vscode from "../assets/VSCode-Dark.svg";
 import "./Skills.css";
 
 class Skills extends Component {
@@ -6,6 +15,35 @@ class Skills extends Component {
     return (
       <div className="skills_section">
         <h2>MY ECOSYSTEM</h2>
+        <div>
+          <span>
+            <img src="" alt="" />
+          </span>
+          <span>
+            <img src="" alt="" />
+          </span>
+          <span>
+            <img src="" alt="" />
+          </span>
+          <span>
+            <img src="" alt="" />
+          </span>
+          <span>
+            <img src="" alt="" />
+          </span>
+          <span>
+            <img src="" alt="" />
+          </span>
+          <span>
+            <img src="" alt="" />
+          </span>
+          <span>
+            <img src="" alt="" />
+          </span>
+          <span>
+            <img src="" alt="" />
+          </span>
+        </div>
       </div>
     );
   }
