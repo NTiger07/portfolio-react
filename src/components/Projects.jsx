b73c180f3a5ce59a17bb9324e61f2d3001d569c2
NTiger07 <falaleru@gmail.com>
2023-01-15 23:39:49 +0100

    Projects skeleton gspeed

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -3,7 +3,27 @@ import "./Projects.css";
 
 class Projects extends Component {
   render() {
-    return <div className="projects_section"></div>;
+    return (
+      <div className="projects_section">
+        <h1>My Projects</h1>
+        <div className="projects_container">
+          <div className="project_card">
+            <img src="" alt="" />
+            <span></span>
+            <p></p>
+            <span>Built with</span>
+            <div className="project_links">
+              <button>
+                <a href=""></a>
+              </button>
+              <button>
+                <a href=""></a>
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
