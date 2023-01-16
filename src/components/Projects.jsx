25dcc0016115c088fd3da8f340536ff244d9263f
NTiger07 <falaleru@gmail.com>
2023-01-16 23:37:17 +0100

    Projects skeleton update

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -9,29 +9,34 @@ class Projects extends Component {
         <div className="projects_container">
           <div className="project_card">
             <div className="project_image">
-              <img src="" alt="" />
+              <img src="" alt="projectimage" />
             </div>
             <div className="project_content">
               <span>
-                <b></b>
+                <b>LIGMA</b>
               </span>
-              <p></p>
+              <p>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illo
+                itaque quos beatae, veritatis quidem dolores a! Placeat rerum
+                vero quia assumenda temporibus, corporis, consequuntur ullam,
+                molestiae est laborum earum. Recusandae.
+              </p>
               <div className="project_tools">
                 <span>
                   <b>Built with:</b>
                 </span>
                 <div className="tools_content">
-                  <span></span>
-                  <span></span>
-                  <span></span>
+                  <span>Que</span>
+                  <span>Miras</span>
+                  <span>Bobo</span>
                 </div>
               </div>
               <div className="project_links">
                 <button>
-                  <a href=""></a>
+                  <a href="">View Repo</a>
                 </button>
                 <button>
-                  <a href=""></a>
+                  <a href="">View Website</a>
                 </button>
               </div>
             </div>
