d3c57d7aae0da06186d742e059d660edb9a2e2a4
NTiger07 <falaleru@gmail.com>
2023-01-20 23:44:21 +0100

    project multiply

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -41,6 +41,108 @@ class Projects extends Component {
               </div>
             </div>
           </div>
+          <div className="project_card">
+            <div className="project_image">
+              <img src="" alt="projectimage" />
+            </div>
+            <div className="project_content">
+              <span className="project-title">
+                <b>QUE MIRAS BOBO</b>
+              </span>
+              <p>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illo
+                itaque quos beatae, veritatis quidem dolores a! Placeat rerum
+                vero quia assumenda temporibus, corporis, consequuntur ullam,
+                molestiae est laborum earum. Recusandae.
+              </p>
+              <div className="project_tools">
+                <span>
+                  <b>Built with:</b>
+                </span>
+                <div className="tools_content">
+                  <span>Que</span>
+                  <span>Miras</span>
+                  <span>Bobo</span>
+                </div>
+              </div>
+              <div className="project_links">
+                <button>
+                  <a href="">View Repo</a>
+                </button>
+                <button>
+                  <a href="">View Website</a>
+                </button>
+              </div>
+            </div>
+          </div>
+          <div className="project_card">
+            <div className="project_image">
+              <img src="" alt="projectimage" />
+            </div>
+            <div className="project_content">
+              <span className="project-title">
+                <b>QUE MIRAS BOBO</b>
+              </span>
+              <p>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illo
+                itaque quos beatae, veritatis quidem dolores a! Placeat rerum
+                vero quia assumenda temporibus, corporis, consequuntur ullam,
+                molestiae est laborum earum. Recusandae.
+              </p>
+              <div className="project_tools">
+                <span>
+                  <b>Built with:</b>
+                </span>
+                <div className="tools_content">
+                  <span>Que</span>
+                  <span>Miras</span>
+                  <span>Bobo</span>
+                </div>
+              </div>
+              <div className="project_links">
+                <button>
+                  <a href="">View Repo</a>
+                </button>
+                <button>
+                  <a href="">View Website</a>
+                </button>
+              </div>
+            </div>
+          </div>
+          <div className="project_card">
+            <div className="project_image">
+              <img src="" alt="projectimage" />
+            </div>
+            <div className="project_content">
+              <span className="project-title">
+                <b>QUE MIRAS BOBO</b>
+              </span>
+              <p>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illo
+                itaque quos beatae, veritatis quidem dolores a! Placeat rerum
+                vero quia assumenda temporibus, corporis, consequuntur ullam,
+                molestiae est laborum earum. Recusandae.
+              </p>
+              <div className="project_tools">
+                <span>
+                  <b>Built with:</b>
+                </span>
+                <div className="tools_content">
+                  <span>Que</span>
+                  <span>Miras</span>
+                  <span>Bobo</span>
+                </div>
+              </div>
+              <div className="project_links">
+                <button>
+                  <a href="">View Repo</a>
+                </button>
+                <button>
+                  <a href="">View Website</a>
+                </button>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     );
