c7375a8210a5f7536ca5a875d1b22ac613db7f40
NTiger07 <falaleru@gmail.com>
2023-01-24 22:35:17 +0100

    footer gspeed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,11 +5,12 @@ export default function Footer() {
   return (
     <div className="footer_section">
       <span>
-        Want me building stuff for you? Hit me on LinkedIn{" "}
+        Want me building stuff for you? Hit me on
         <button>
           <a href="https://www.linkedin.com/in/favourolaleru/">LinkedIn</a>
         </button>
       </span>
+      <div>Follow me on</div>
     </div>
   );
 }
