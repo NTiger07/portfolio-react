90c81df5d5a1cedec2ba724bb87f721ceb05e7e2
NTiger07 <falaleru@gmail.com>
2023-01-26 08:35:27 +0100

    Added spinner

---
+++ src/components/LoadingSpinner.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./LoadingSpinner.css";
+
+export default function LoadingSpinner() {
+  return (
+    <div class="cssload-loader">
+      <div class="cssload-inner cssload-one"></div>
+      <div class="cssload-inner cssload-two"></div>
+      <div class="cssload-inner cssload-three"></div>
+    </div>
+  );
+}
