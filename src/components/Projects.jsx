4b6a3238c0a728a6960b060b3b8961e802c47720
NTiger07 <falaleru@gmail.com>
2023-01-30 23:27:13 +0100

    Weather project added

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -66,11 +66,11 @@ class Projects extends Component {
                 <b>Weather App</b>
               </span>
               <p>
-                The recipe app is a web-based application built with React. It
-                utilizes the Edemam API to retrieve and display recipes based on
-                the user search queries. The app features a clean and intuitive
-                user interface that allows users to easily browse and search for
-                recipes by ingredients, diet, cuisine origin and meal type.
+                This Weather App is an easy and convenient way to quickly check
+                the current temperature, description, humidity and wind speed of
+                any city or country. Using the OpenWeatherMap API users can
+                easily search for current weather conditons of any city. Made
+                with Vanilla JavaScript and deployed to Netlify.
               </p>
               <div className="project_tools">
                 <span>
