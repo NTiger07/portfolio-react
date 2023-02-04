beea6a6b87f1aee8670e9bfee0ddd3d26c8b6640
NTiger07 <falaleru@gmail.com>
2023-02-04 22:58:59 +0100

    hamburger menu logic

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,30 +1,38 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import hamburger from "../svgs/hamburger.svg";
 import "./Navbar.css";
 
-export default class Navbar extends Component {
-  render() {
-    return (
-      <div className="navbar_container">
-        <div className="logo_container">
-          <a href="/">Favour Olaleru</a>
-        </div>
-        <div className="navlinks_container">
-          <ul className="nav-list">
-            <li className="nav-links">
-              <Link to="/">Home</Link>
-            </li>
-            <li className="nav-links">
-              <Link to="/about">About Me</Link>
-            </li>
-            <li className="nav-links">
-              <Link to="/skills">Skills</Link>
-            </li>
-            <li className="nav-links">
-              <Link to="/contact">Contact</Link>
-            </li>
-          </ul>
-        </div>
+export default function Navbar() {
+  const [navOpen, setNavOpen] = React.useState(false);
+  const toggle = () => {
+    setNavOpen(!navOpen);
+  };
+  return (
+    <div className="navbar_container">
+      <div className="logo_container">
+        <a href="/">Favour Olaleru</a>
+      </div>
+      <div className="navlinks_container">
+        <ul className="nav-list">
+          <li className="nav-links">
+            <Link to="/">Home</Link>
+          </li>
+          <li className="nav-links">
+            <Link to="/about">About Me</Link>
+          </li>
+          <li className="nav-links">
+            <Link to="/skills">Skills</Link>
+          </li>
+          <li className="nav-links">
+            <Link to="/contact">Contact</Link>
+          </li>
+        </ul>
+      </div>
+      <div className="ham_image" onClick={toggle}>
+        <img src={hamburger} alt="hamburger" />
+      </div>
+      {navOpen && (
         <div className="navlinks_mobile">
           <ul className="nav-list">
             <li className="nav-links">
@@ -41,7 +49,7 @@ export default class Navbar extends Component {
             </li>
           </ul>
         </div>
-      </div>
-    );
-  }
+      )}
+    </div>
+  );
 }
