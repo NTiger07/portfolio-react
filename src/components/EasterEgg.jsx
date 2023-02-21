ec904541d086dc2620bdd24df034e2a22769db93
NTiger07 <falaleru@gmail.com>
2023-02-21 16:44:44 +0100

    easter egg logic done

--- src/components/EasterEgg.jsx
+++ src/components/EasterEgg.jsx
@@ -6,16 +6,36 @@ export default function EasterEgg(props) {
     const elementhero = document.getElementById("scrollhero");
     const elementabout = document.getElementById("scrollabout");
     const elementproj = document.getElementById("scrollproj");
-    elementhero.scrollIntoView({ behavior: "smooth" });
-    elementabout.scrollIntoView({ behavior: "smooth" });
-    elementproj.scrollIntoView({ behavior: "smooth" });
+    const elementeaster = document.getElementById("scrolleast");
+
+    const linesOfCode = [
+      "elementhero.scrollIntoView({ behavior: 'smooth' })",
+      "elementproj.scrollIntoView({ behavior: 'smooth' })",
+      "elementabout.scrollIntoView({ behavior: 'smooth' })",
+      "elementeaster.scrollIntoView({ behavior: 'smooth' })",
+    ];
+
+    function executeCodeWithIntervals() {
+      let i = 0;
+      const intervalID = setInterval(() => {
+        eval(linesOfCode[i]);
+        i++;
+        if (i >= linesOfCode.length) {
+          clearInterval(intervalID);
+        }
+      }, 2000);
+    }
+
     props.setIsVanished(!props.isVanished);
     setTimeout(() => {
       props.setIsVanished(false);
-    }, 24000);
+    }, 12500);
+
+    executeCodeWithIntervals();
   };
+
   return (
-    <div className="easteregg_section">
+    <div className="easteregg_section" id="scrolleast">
       <button onClick={handleSnap}>Click for Easter Egg</button>
       <span className={`${props.isVanished ? "fade-out" : " "}`}>
         I love you
