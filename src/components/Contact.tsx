cbb3675559fc3924aaf9abf55a3b40f58e9915b4
NTiger07 <falaleru@gmail.com>
2025-03-30 20:48:53 +0100

    added contact content

---
+++ src/components/Contact.tsx
@@ -0,0 +1,15 @@
+const Contact = () => {
+return(
+    <div className="flex flex-col">
+        <h2>Contact</h2>
+        <div className="flex flex-col" >
+            <span><strong>Email:</strong> <a href="mailto:falaleru@gmail.com">falaleru@gmail.com</a></span>
+            <span><strong>GitHub:</strong> <a href="https://github.com/NTiger07" target="_blank">github.com/NTiger07</a></span>
+            <span><strong>LinkedIn:</strong> <a href="YOUR_LINKEDIN_URL" target="_blank">Your LinkedIn</a></span>
+        </div>
+
+    </div>
+    )
+}
+
+export default Contact
